dab2d138047aca174391034381543210900547df
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-07-25 13:42:52 +0530

    add button and text view

--- Tutorial-1/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Tutorial-1/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    val button = R.id.button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
