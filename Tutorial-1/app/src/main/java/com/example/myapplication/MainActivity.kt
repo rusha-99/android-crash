0ed538366f9520455824991375b28c3c6687b077
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-07-25 13:58:19 +0530

    Update MainActivity.kt

--- Tutorial-1/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Tutorial-1/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,15 +2,27 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    val button = R.id.button
+  
 
+    private var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myButton: Button = findViewById(R.id.button)
+        val myText:TextView = findViewById(R.id.textView)
+        myButton.setOnClickListener {
+            // Button click action here
+            counter++
+            myText.text = counter.toString()
+            // Place the code you want to execute when the button is clicked.
+        }
 
 
     }
